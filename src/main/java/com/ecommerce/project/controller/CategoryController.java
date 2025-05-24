d48bb264905157f7ff5dda00a86a5f2851a9db46
devErikyLS <erikylimas2@gmail.com>
2025-05-24 14:12:33 -0300

    Validations in Spring

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -3,14 +3,11 @@ package com.ecommerce.project.controller;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -36,21 +33,13 @@ public class CategoryController {
 
     @DeleteMapping("/admin/categories/{id}")
     public ResponseEntity <String> deleteCategory(@PathVariable("id") Long categoryId) {
-        try {
-            String status = categoryService.deleteCategory(categoryId);
-            return new ResponseEntity<>(status, HttpStatus.OK);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+        String status = categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(status, HttpStatus.OK);
     }
 
     @PutMapping("/public/categories/{categoryId}")
-    public ResponseEntity <String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId) {
-        try {
-            Category saveCategory = categoryService.updateCategory(category, categoryId);
-            return new ResponseEntity<>("Category with categoryID: " + categoryId, HttpStatus.OK);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+    public ResponseEntity <String> updateCategory(@Valid @RequestBody Category category, @PathVariable Long categoryId) {
+        Category saveCategory = categoryService.updateCategory(category, categoryId);
+        return new ResponseEntity<>("Category with categoryID: " + categoryId, HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.exceptions;
 
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -23,6 +24,18 @@ public class MyGlobalExceptionHandler {
         });
         return new ResponseEntity<Map<String, String>>(response,
                 HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity <String> myResourceNotFoundException(ResourceNotFoundException e) {
+        String message = e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity <String> myAPIException(APIException e) {
+        String message = e.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 
 }
